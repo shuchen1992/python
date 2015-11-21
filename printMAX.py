01fe7c23a552137358ebe483c84a42175f1fc26e
shuchen1992 <610244213@qq.com>
2015-11-21 11:55:21 +0800

    initial commit

---
+++ printMAX.py
@@ -0,0 +1,24 @@
+class exercise(object):
+    def __init__(self):
+        pass
+
+    
+    def printMAX(self,a,b):
+        while 1:
+            if a>b:
+                print 'the max num is %d'%a
+                break
+            elif a==b:
+                print 'the two number is the same'
+                break
+            else:
+                print 'the max num is %d'%b
+                break
+
+    def phonenumber(self,a,b):
+        lianxiren={}
+        lianxiren[a]=b
+        print lianxiren[a]
+    def Person():
+        pass
+    
